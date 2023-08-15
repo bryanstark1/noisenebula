62b6fb82e1e4a2941d37627930f080b2cb363e2e
bryanstark1 <bryandstark@icloud.com>
2023-08-15 13:12:02 -0400

    Deployment

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use('/songs', songRouter);
 app.use('/users', userRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 });
 
 
